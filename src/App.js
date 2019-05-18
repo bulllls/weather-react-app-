ca778c1b440d70572cf74872759749c2a1afe5ce
Ruslan <ruslanfilistovich@gmail.com>
2019-05-18 14:50:08 +0300

    work with components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Info from "./components/info";
 import './App.css';
 class App extends React.Component {
   render() {
     return (
-//Возвращает только один div
       <div>
-      <div>Hi test </div>
-      <h1>Hi test2 </h1>
+        <Info/>
       </div>
     );
   }
 }
 
-export default App;//Компонент нужно экспортировать
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
+export default App;
