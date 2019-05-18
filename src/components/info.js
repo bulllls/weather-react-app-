f6f7dc32824d50f18f37cff18bdeca53e06ada2e
Ruslan <ruslanfilistovich@gmail.com>
2019-05-18 16:50:00 +0300

    Оптимизация кода

--- src/components/info.js
+++ src/components/info.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-class Info extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2> Погодное приложение </h2>
-        <p> Узнайте погоду в Вашем городе</p>
-      </div>
-
-    );
-  }
-}
+const Info = props => (
+  <div>
+    <h2> Погодное приложение </h2>
+    <p> Узнайте погоду в Вашем городе</p>
+  </div>
+)
 
 export default Info;
